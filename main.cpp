d958382ac788767fcbb783d14cd9bdb79d09e929
Yang Le <yangle0125@qq.com>
2020-05-10 21:13:40 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,110 @@
+﻿#include <thread>
+#include <chrono>
+
+#include <glad/glad.h> 
+// Include glfw3.h after our OpenGL definitions
+#include <GLFW/glfw3.h>
+
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
+using namespace std::chrono_literals;
+
+int main(void)
+{
+    /* Initialize the library */
+    if (!glfwInit())
+        return 1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL4 example", NULL, NULL);
+    if (!window)
+        return 1;
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGL())
+        return 1;
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init();
+
+    const int image_width = 256;
+    const int image_height = 256;
+    unsigned char image_data[3 * image_width * image_height];
+
+    for (int j = 0; j < image_height; ++j) {
+        for (int i = 0; i < image_width; ++i) {
+            auto r = double(i) / (image_width - 1);
+            auto g = double(image_height - 1 - j) / (image_height - 1);
+            auto b = 0.25;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            image_data[3 * (j * image_width + i) + 0] = ir;
+            image_data[3 * (j * image_width + i) + 1] = ig;
+            image_data[3 * (j * image_width + i) + 2] = ib;
+        }
+    }
+
+    // Create a OpenGL texture identifier
+    GLuint image_texture;
+    glGenTextures(1, &image_texture);
+    glBindTexture(GL_TEXTURE_2D, image_texture);
+
+    // Setup filtering parameters for display
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // Upload pixels into texture
+    glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image_width, image_height, 0, GL_RGB, GL_UNSIGNED_BYTE, image_data);
+
+    bool show_demo_window = false;
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Poll for and process events */
+        glfwPollEvents();
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("OpenGL Texture Text");
+        ImGui::Checkbox("Demo Window", &show_demo_window);
+
+        ImGui::Text("pointer = %p", image_texture);
+        ImGui::Text("size = %d x %d", image_width, image_height);
+        ImGui::Image((void*)(intptr_t)image_texture, ImVec2(image_width, image_height));
+        ImGui::End();
+
+        if (show_demo_window)
+            ImGui::ShowDemoWindow();
+
+        // Rendering
+        ImGui::Render();
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        std::this_thread::sleep_for(20ms);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwTerminate();
+    return 0;
+}
